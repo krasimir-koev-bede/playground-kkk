2f0f7f8e39b5ab30d56e7bd4b4625a1c47d30f02
Krasimir Koev <krasimir.koev@bedegaming.com>
2021-09-26 20:20:15 +0300

    feat: first vm :)

---
+++ vm.tf
@@ -0,0 +1,71 @@
+
+resource "azurerm_public_ip" "publicIp1" {
+  name = "${var.GLOBAL_RESOURCENAME_PREFIX}publicIp1"
+  location = var.GLOBAL_LOCATION
+  resource_group_name = azurerm_resource_group.example_resourcegroup.name
+  allocation_method = "Static"
+  ip_version = "IPv4"
+
+}
+
+
+resource "azurerm_network_interface" "main_nic" {
+  name = "${var.GLOBAL_RESOURCENAME_PREFIX}nic"
+  location = azurerm_resource_group.example_resourcegroup.location
+  resource_group_name = azurerm_resource_group.example_resourcegroup.name
+
+  ip_configuration {
+    name = "${var.GLOBAL_RESOURCENAME_PREFIX}nic_ip"
+    subnet_id = azurerm_subnet.subnet_1.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.publicIp1.id
+  }
+  
+}
+
+
+
+
+resource "azurerm_virtual_machine" "vm1" {
+  name = "${var.GLOBAL_RESOURCENAME_PREFIX}vm1"
+  location = azurerm_resource_group.example_resourcegroup.location
+  resource_group_name = azurerm_resource_group.example_resourcegroup.name
+
+  network_interface_ids = [ azurerm_network_interface.main_nic.id ]
+  vm_size = "Standard_B1s"
+
+  storage_image_reference {
+    publisher = "OpenLogic"
+    offer = "Centos"
+    sku = "7.5"
+    version = "latest"
+  }
+
+  storage_os_disk {
+    name = "${var.GLOBAL_RESOURCENAME_PREFIX}osdisk"
+    caching = "ReadWrite"
+    create_option = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name = "vm1"
+    admin_username = "krasi"
+    admin_password = "Krasi123"
+
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+
+}
+
+
+output "vm_ip_priv" {
+  value = azurerm_network_interface.main_nic.private_ip_address
+}
+
+output "vm_ip_pub" {
+  value = azurerm_public_ip.publicIp1.ip_address
+}
